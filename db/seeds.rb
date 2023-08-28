8676a83e8aa47e8b18dc51188da4080f9afdbc49
diego joao <diegosgjoao@gmail.com>
2023-08-28 15:46:15 -0300

    seed criada

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Place.destroy_all
+User.destroy_all
+
+user = User.create!(email: 'teste@teste.com', password: '123123')
+
 Place.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   number_of_guests: 3,
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
-  price: 75
+  price: 75,
+  user: user
 )
 
 Place.create!(
@@ -20,7 +19,8 @@ Place.create!(
   number_of_guests: 3,
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
-  price: 75
+  price: 75,
+  user: user
 )
 
 Place.create!(
@@ -29,5 +29,6 @@ Place.create!(
   number_of_guests: 3,
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
-  price: 75
+  price: 75,
+  user: user
 )
