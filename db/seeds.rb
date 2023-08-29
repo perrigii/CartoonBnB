f084c533fe239cd18157dc43a2211cef241872b6
André ÃLacerda <dedelacerda95@gmail.com>
2023-08-29 17:16:29 -0300

    coloquei 3 seeds e redimencionei imagens

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,31 +4,32 @@ User.destroy_all
 user = User.create!(email: 'teste@teste.com', password: '123123')
 
 Place.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
+  name: "Squidward's house",
+  address: "Bikini Bottom, Sponge Bob's universe",
   number_of_guests: 3,
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
-  price: 75,
+  description: "A beutiful house with many rooms and floors. Perfect for art enthusiasts.",
+  picture_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.NUgD8XJbNMyI0yvXXpX5lQHaFk%26pid%3DApi&f=1&ipt=3061db28d4d74d7b52c99810b283d2d527bb1b55dd2b477c29d6481f8dbbd26a&ipo=images',
+  price: 300,
   user: user
 )
 
 Place.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  number_of_guests: 3,
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
-  price: 75,
+  name: "Sponge Bob's house",
+  address: "Bikini Bottom, Sponge Bob's universe",
+  number_of_guests: 2,
+  description: "It's a pineapple under the sea.",
+  picture_url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg3.wikia.nocookie.net%2F__cb20150425071924%2Fspongebob%2Fimages%2F0%2F0e%2FSpongeBob%27s_pineapple_house_in_Season_3-3.png&f=1&nofb=1&ipt=d47f45f69fbdd4f4bc1e535cfcb98742dd4e4729c6d1095e0c7e1e8847356891&ipo=images',
+  price: 250,
   user: user
 )
 
+
 Place.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  number_of_guests: 3,
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
-  price: 75,
+  name: "Patrick Star's house",
+  address: "Bikini Bottom, Sponge Bob's universe",
+  number_of_guests: 1,
+  description: "A simple accomodating ambient under a rock.",
+  picture_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffsa.zobj.net%2Fcrop.php%3Fr%3DEAUHT5GB-mE6NPMeUXdFXLTz6ItF4oC9931LZ9xYxJFRbbcUqPwbViXLgvlBc4BCq2LX8QJpWiRjoVOLLER-CE3kHS6Lekg8bYGTUOxjxeZxesSSlOBtusirHkXbfjkMpiJHen-ZS0f-ul61&f=1&nofb=1&ipt=550d5fd2596c1aa7ca47439b55f2112433233ec0eaaabe83787d5dbd6e0b1a58&ipo=images',
+  price: 100,
   user: user
 )
