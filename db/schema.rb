a9f3d4841ef5952bbc019196e63785e9c96706b9
diego joao <diegosgjoao@gmail.com>
2023-08-29 12:20:11 -0300

    modelo order criado e controller gerado, mas ainda vazio

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_184218) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_151155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "place_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "comment"
+    t.index ["place_id"], name: "index_orders_on_place_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -44,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_184218) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "places"
+  add_foreign_key "orders", "users"
   add_foreign_key "places", "users"
 end
