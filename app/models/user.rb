c9000dde948cc6101f81c05ca9c0d3267035ab61
diego joao <diegosgjoao@gmail.com>
2023-08-31 14:19:05 -0300

    validações para campos do model user feitas

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :places, dependent: :destroy
   has_many :orders, dependent: :destroy
+  validates :username, presence: true, uniqueness: true
+  validates :full_name, presence: true, format: { with: /\A(\w+\s\w+)\z/, message: "should have at least 2 names" }
+  validates :description, presence: true
 end
